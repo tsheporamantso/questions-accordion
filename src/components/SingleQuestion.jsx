58b3738399e472d9ab6ab1b56a69b953b7752b20
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-23 14:07:04 +0200

    Implement toggle functionality in SingleQuestion component

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const TOGGLE_BTN = "TOGGLE_BTN";
+
+const defaultState = {
+  showInfo: false,
+};
+
+const reducer = (state, action) => {
+  if (action.type === TOGGLE_BTN) {
+    return { ...state, showInfo: !state.showInfo };
+  }
+  return state;
+};
 
 const SingleQuestion = ({ info, title }) => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
+        <button
+          className="question-btn"
+          onClick={() => dispatch({ type: TOGGLE_BTN })}
+        >
+          {state.showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </header>
-      <p>{info}</p>
+      {state.showInfo && <p>{info}</p>}
     </article>
   );
 };
